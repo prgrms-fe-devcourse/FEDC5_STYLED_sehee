d940b29d1b1d8408093832e966630ecb6b3ce432
지인혁 <dhgg321@naver.com>
2024-01-01 11:56:57 +0900

    Feat: Theme 테마에 따른 background 색상 추가

--- src/Styles/Theme.ts
+++ src/Styles/Theme.ts
@@ -6,14 +6,12 @@ const commonTheme = {
   alert: '#FF0000', // 알림 색상
   border: '#CCCCCC', // 보더 색상
   backgroundGrey: '#D1CCC7', // 회색 배경 색상
-  backgroundWhite: '#FFFFFF', // 배경 흰색 색상
   focusHover: '#F2F2F2', // 포커스 및 호버 색상
   focusHoverText: '#000000',
   overlay: 'rgba(0, 0, 0, 0.4)', // 오버레이 색상
   buttonClickHover: 'rgba(0, 0, 0, 0.3)', // 버튼 클릭 시 호버 색상
   follow: 'rgba(119, 82, 254, 1)', // 팔로우 색상
 };
-
 const size = {
   extraSmall: '0.5rem',
   small: '1rem',
@@ -25,17 +23,16 @@ const size = {
   half: '50%',
   none: '0%',
 };
-
 const device = {
   mobile: `(max-width: 480px)`,
   tablet: `(max-width: 768px)`,
   laptop: `(max-width: 1024px)`,
 };
-
 export const lightTheme: DefaultTheme = {
   colors: {
     primary: '#000000', // 메인 색상
     text: '#000000', // 글자 색상
+    background: '#FFFFFF', // 배경 색상
     buttonText: '#FFFFFF', // 버튼 텍스트 색상
     buttonBackground: '#000000', // 버튼 배경 색상
     ...commonTheme,
@@ -43,11 +40,11 @@ export const lightTheme: DefaultTheme = {
   size,
   device,
 };
-
 export const darkTheme: DefaultTheme = {
   colors: {
     primary: '#FFFFFF', // 메인 색상
     text: '#FFFFFF', // 글자 색상
+    background: '#000000', // 배경 색상
     buttonText: '#000000', // 버튼 텍스트 색상
     buttonBackground: '#FFFFFF', // 버튼 배경 색상
     ...commonTheme,
@@ -55,5 +52,4 @@ export const darkTheme: DefaultTheme = {
   size,
   device,
 };
-
 export type ThemeTypes = typeof lightTheme;
