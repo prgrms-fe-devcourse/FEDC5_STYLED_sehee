f39f28909e8a97ae597875a81f8593aeca000ddd
Cho-Ik-Jun <harry960629@naver.com>
2024-01-02 02:42:28 +0900

    Feat: ImageUpload 내부 이미지 업로드 로직 구현

--- src/Components/Common/ImageUpload/index.tsx
+++ src/Components/Common/ImageUpload/index.tsx
@@ -1,24 +1,87 @@
-import { useRef } from 'react';
+import { ChangeEvent, MouseEvent, useMemo, useRef, useState } from 'react';
 import Icon from '@/Components/Base/Icon';
-import { StyledInput, StyledText, StyledWrapper } from './style';
-import Button from '@/Components/Base/Button';
+import {
+  StyledImage,
+  StyledInput,
+  StyledText,
+  StyledWrapper,
+  StyledUploadForm,
+  StyledButton,
+  IconStyle,
+} from './style';
+import { ImageFileType, ImageUploadProps } from './type';
 
-const ImageUpload = () => {
+const ImageUpload = ({ width, height, fontSize }: ImageUploadProps) => {
   const uploadInput = useRef<HTMLInputElement>(null);
+  const [imageFile, setImageFile] = useState<ImageFileType | null>(null);
+
+  /**
+   * 파일 업로드 버튼 클릭 핸들러 함수
+   * 다른 요소에서 클릭 이벤트에 매핑하면 파일 업로드 진행됨
+   */
+  const handleClickUpload = (e: MouseEvent) => {
+    e.preventDefault();
+
+    uploadInput.current?.click();
+  };
+
+  /**
+   * 업로드 이미지 파일 상태로 저장하는 함수
+   * 파일의 url을 URL.createObjectURL로 변환하여 저장
+   */
+  const uploadImage = (e: ChangeEvent<HTMLInputElement>) => {
+    const ImageFileData = e.target.files;
+    const isFileEmpty = ImageFileData?.length === 0;
+
+    if (ImageFileData && !isFileEmpty) {
+      const uploadImageUrl = URL.createObjectURL(ImageFileData[0]);
+
+      setImageFile({
+        file: ImageFileData[0],
+        imageUrl: uploadImageUrl,
+        type: ImageFileData[0].type.slice(0, 5),
+      });
+    }
+  };
+
+  /**
+   * 업로드한 이미지가 있는지에 따라 업로드 폼과 이미지를 교체하는 함수
+   * useMemo를 사용한 것은 이미지 업로드와 렌더링을 최적화하기 위함
+   */
+  const showUploadImage = useMemo(() => {
+    return imageFile ? (
+      <StyledImage
+        src={imageFile.imageUrl}
+        alt={imageFile.type}
+        onClick={handleClickUpload}
+      />
+    ) : null;
+  }, [imageFile]);
 
   return (
-    <StyledWrapper>
-      <Icon
-        name="add_a_photo"
-        onClick={() => console.log('icon 클릭')}
-      />
-      <StyledText>사진을 선택하여 추가해주세요.</StyledText>
-      <Button onClickButton={() => console.log('button 클릭')}>가져오기</Button>
-      <StyledInput
-        type="file"
-        accept="image/*"
-        ref={uploadInput}
-      />
+    <StyledWrapper
+      $width={width}
+      $height={height}
+      $fontSize={fontSize}
+    >
+      {showUploadImage}
+      <StyledUploadForm style={{ display: imageFile ? 'none' : 'flex' }}>
+        <Icon
+          name="add_a_photo"
+          onClick={handleClickUpload}
+          style={{ fontSize: fontSize && fontSize * 50, ...IconStyle }}
+        />
+        <StyledText>사진을 선택하여 추가해주세요.</StyledText>
+        {/* 버튼 컴포넌트 handleClick에 e.preventDefault 필요 */}
+        {/* <Button onClickButton={handleClickUpload}>가져오기</Button> */}
+        <StyledButton onClick={handleClickUpload}>가져오기</StyledButton>
+        <StyledInput
+          type="file"
+          accept="image/*"
+          ref={uploadInput}
+          onChange={uploadImage}
+        />
+      </StyledUploadForm>
     </StyledWrapper>
   );
 };
