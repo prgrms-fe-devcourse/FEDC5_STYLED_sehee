8126330f660f7f2f473786fd2bff37535a8d82a3
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-01-02 13:27:15 +0900

    Feat: Icon 컴포넌트에 ref 추가 (#25)
    * Fix: 채워진 아이콘 관련 수정사항
    
    * Fix: Icon에 theme 추가
    
    * Fix: outline 피드백 반영
    
    * Feat: Icon 컴포넌트 ref 추가
    
    * Fix: Icon 컴포넌트 ref 수정 (테스트 완료)
    
    * Feat: className 대신 isFill prop으로 변경
    
    * Comment: 주석 제거

--- src/Components/Base/Icon/index.tsx
+++ src/Components/Base/Icon/index.tsx
@@ -1,18 +1,24 @@
+import { forwardRef, ForwardedRef } from 'react';
 import StyledIconContainer from './style';
 import IconProps from './type';
 
-/**
- * 채워지지 않은 아이콘 => className = 'material-symbols-outlined'로 지정
- */
-const Icon = ({ name, className = 'material-icons', ...props }: IconProps) => {
-  return (
-    <StyledIconContainer
-      {...props}
-      className={className}
-    >
-      {name}
-    </StyledIconContainer>
-  );
-};
+const Icon = forwardRef(
+  (
+    { name, isFill = true, ...props }: IconProps,
+    ref: ForwardedRef<HTMLSpanElement>,
+  ) => {
+    return (
+      <StyledIconContainer
+        {...props}
+        ref={ref}
+        className={isFill ? 'material-icons' : 'material-symbols-outlined'}
+      >
+        {name}
+      </StyledIconContainer>
+    );
+  },
+);
+
+Icon.displayName = 'Icon';
 
 export default Icon;
