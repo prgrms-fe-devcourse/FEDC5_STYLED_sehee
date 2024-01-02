7ffddeff2b97ba40477a02bb94bcd0c731e28f71
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-01-02 09:44:17 +0900

    Fix: Icon 컴포넌트 outline관련 피드백 반영 (#21)
    * Fix: 채워진 아이콘 관련 수정사항
    
    * Fix: Icon에 theme 추가
    
    * Fix: outline 피드백 반영

--- src/Components/Base/Icon/index.tsx
+++ src/Components/Base/Icon/index.tsx
@@ -1,6 +1,9 @@
 import StyledIconContainer from './style';
 import IconProps from './type';
 
+/**
+ * 채워지지 않은 아이콘 => className = 'material-symbols-outlined'로 지정
+ */
 const Icon = ({ name, className = 'material-icons', ...props }: IconProps) => {
   return (
     <StyledIconContainer
