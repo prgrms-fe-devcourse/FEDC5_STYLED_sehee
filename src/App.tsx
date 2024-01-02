13365d7a7da948184b2318c1fd9c72978d3ea939
kim-hyunjoo <khj9x.x@gmail.com>
2024-01-02 16:05:20 +0900

    Refactor: App 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
 import { ThemeProvider } from 'styled-components';
-import { useState } from 'react';
 import { lightTheme } from '@/Styles/Theme';
 import GlobalStyle from '@/Styles/Global';
 import RouteManager from '@/Routes/Router';
-import Alert from './Components/Common/Alert';
 
 const App = () => {
-  const [isShow, setIsShow] = useState(false);
   return (
     <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <RouteManager />
-      <button
-        type="button"
-        onClick={() => setIsShow(true)}
-      >
-        test버튼
-      </button>
-      {isShow && (
-        <Alert
-          onChangeOpen={setIsShow}
-          message="정말 삭제하시겠습니까?"
-          mode="confirm"
-        />
-      )}
     </ThemeProvider>
   );
 };
