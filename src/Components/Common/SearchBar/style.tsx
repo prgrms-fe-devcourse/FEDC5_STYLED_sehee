0543fbc2c28a05f302c2b7e7b03518ab809c2f2a
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-01-02 13:27:42 +0900

    Feat: SearchBar 컴포넌트 (#18)
    * Feat: SearchBar 초기 레이아웃
    
    * Feat: 버튼 컴포넌트 사용
    
    * Refactor: 검색 기능 중간 점검
    
    * Feat: SearchBar
    
    * Feat: SearchBar 컴포넌트
    
    * Feat: props, iconProps, inputProps 추가

---
+++ src/Components/Common/SearchBar/style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.form`
+  display: flex;
+  align-items: center;
+  left: 50%;
+  position: fixed;
+`;
+
+export default StyledContainer;
