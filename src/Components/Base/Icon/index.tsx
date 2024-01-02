0feb0aa1ef301bd435de9542c14fa557c2ddc318
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-01-02 13:24:16 +0900

    Comment: 주석 제거

--- src/Components/Base/Icon/index.tsx
+++ src/Components/Base/Icon/index.tsx
@@ -2,9 +2,6 @@ import { forwardRef, ForwardedRef } from 'react';
 import StyledIconContainer from './style';
 import IconProps from './type';
 
-/**
- * 채워지지 않은 아이콘 => className = 'material-symbols-outlined'로 지정
- */
 const Icon = forwardRef(
   (
     { name, isFill = true, ...props }: IconProps,
