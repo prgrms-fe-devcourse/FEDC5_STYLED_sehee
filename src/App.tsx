75c74441e24d6aa06080166b3167b2eb23434e43
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-01-03 11:36:24 +0900

    Feat: BrowserRouter 위치 main.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
 import { lightTheme } from '@/Styles/Theme';
 import GlobalStyle from '@/Styles/Global';
 import RouteManager from '@/Routes/Router';
@@ -16,11 +15,9 @@ const App = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <GlobalStyle />
-          <Header />
-          <RouteManager />
-        </BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <RouteManager />
       </QueryClientProvider>
     </ThemeProvider>
   );
