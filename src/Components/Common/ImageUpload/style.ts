e778b736aa63b2fe691f97faa3ee94235cdeb22f
Cho_Ik_Jun <harry960629@naver.com>
2024-01-03 13:32:35 +0900

    Feat: ImageUpload 컴포넌트 구현 (#24)
    * Feat: ImageUpload 구조 초기 구현
    
    * Feat: ImageUpload 내부 이미지 업로드 로직 구현
    
    * Style: ImageUpload 컴포넌트 내 스타일 컴포넌트 및 스타일 지정
    
    * Feat: ImageUpload 컴포넌트 내 타입 지정
    
    * Test: 이미지 업로드 임시 파일 저장
    
    * Style: 가져오기 버튼 hover 색상 적용

---
+++ src/Components/Common/ImageUpload/style.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div<{
+  $width: string;
+  $height: string;
+  $fontSize?: number;
+}>`
+  // 영역 표시 위해 남겨둠 필요 시 배경색 재설정
+  background-color: ${({ theme }) => theme.colors.background};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.$width};
+  height: ${(props) => props.$height};
+  font-size: ${(props) => props.$fontSize}rem;
+`;
+
+export const StyledUploadForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const StyledText = styled.p`
+  font-weight: 600;
+  user-select: none;
+`;
+
+export const getButtonStyle = {
+  padding: '1rem 2rem',
+  transition: 'background-color 50ms ease-in-out',
+};
+
+export const StyledImage = styled.img`
+  object-fit: contain;
+  cursor: pointer;
+`;
+
+export const IconStyle = {
+  cursor: 'pointer',
+};
--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import ImageUpload from '@/Components/Common/ImageUpload';
 
 const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
       <Link to="/detail">Detail</Link>
+      <ImageUpload
+        width="50rem"
+        height="50rem"
+        fontSize={2}
+      />
     </>
   );
 };
