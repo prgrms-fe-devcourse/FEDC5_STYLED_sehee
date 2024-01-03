4a9fbe27f54722ad6733167d967b436711597ebf
지인혁 <dhgg321@naver.com>
2024-01-03 10:51:48 +0900

    Feat: 로그인 페이지 기본 레이아웃 및 스타일 추가

---
+++ src/Pages/LoginPage/index.tsx
@@ -0,0 +1,22 @@
+import MoveToSignUpButton from '@/Components/Login/MoveToSignUpButton';
+import { StyledWrap, StyledContainer } from './style';
+import LoginForm from '@/Components/Login/LoginForm';
+import Logo from '@/Components/Common/Logo';
+import HistoryBackButton from '@/Components/Common/HistoryBackButton';
+
+const LoginPage = () => {
+  return (
+    <StyledWrap>
+      <StyledContainer>
+        <HistoryBackButton
+          style={{ position: 'absolute', left: '3rem', top: '3rem' }}
+        />
+        <Logo />
+        <LoginForm />
+        <MoveToSignUpButton />
+      </StyledContainer>
+    </StyledWrap>
+  );
+};
+
+export default LoginPage;
