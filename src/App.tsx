edd4e95359fbe652f3761e4809c50cc5d4131c47
Jaewoong Hwang <noa9925@gmail.com>
2024-01-03 23:27:29 +0900

    Fix: 테스트코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import { ThemeProvider } from 'styled-components';
 import { lightTheme } from '@/Styles/Theme';
 import GlobalStyle from '@/Styles/Global';
 import RouteManager from '@/Routes/Router';
-import Avatar from './Components/Base/Avatar';
-import Badge from './Components/Base/Badge';
 
 const App = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <RouteManager />
-      <Avatar>
-        <Badge>144</Badge>
-      </Avatar>
     </ThemeProvider>
   );
 };
