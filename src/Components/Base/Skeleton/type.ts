318e8cdb2df3e0a6546c3753ffa1329c68e3b786
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2024-01-04 13:04:11 +0900

    Feat: Skeleton 컴포넌트 구현 (#38)
    * Feat: Skeleton 컴포넌트 구현
    
    * Comment: JSDoc 프롭 설명 추가
    
    * Refactor: 키프레임 디렉토리 분리
    
    * Refactor: Paragraph 사이즈 커스텀 및 JSDoc 수정

---
+++ src/Components/Base/Skeleton/type.ts
@@ -0,0 +1,14 @@
+export interface SkeletonBoxProp {
+  width?: string;
+  height?: string;
+}
+
+export interface SkeletonCircleProp {
+  size?: string;
+}
+
+export interface SkeletonParagraphProp {
+  line?: number;
+  width?: string;
+  height?: string;
+}
--- src/Styles/Animation.ts
+++ src/Styles/Animation.ts
@@ -1,5 +1,28 @@
 import { keyframes } from 'styled-components';
 
+export const skeletonZoomIn = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(0.95);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+export const skeletonLoading = keyframes`
+  0% {
+    background-position-x: 100%;
+  }
+  50% {
+    background-position-x: -100%;
+  }
+  100% {
+    background-position-x: -100%;
+    }
+  `;
+
 export const rotate = keyframes`
   0%, 20%, 80%, 100% {
     transform: scale(1);
