a0e859126a744052234e21fc3b8a243a4c0619bf
Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>
2024-01-04 13:01:36 +0900

    Feat: Badge 컴포넌트 구현 (#30)
    * Feat: Badge 컴포넌트 구현 및 Avatar 컴포넌트 수정
    
    * Fix: 단위 비율 수정 및 JSDoc 작성
    
    * Fix: 테스트코드 제거

--- src/Components/Base/Avatar/type.ts
+++ src/Components/Base/Avatar/type.ts
@@ -7,14 +7,18 @@ export interface AvatarProp extends HTMLAttributes<HTMLDivElement> {
   shape?: 'circle' | 'round' | 'square';
   mode?: 'cover' | 'contain' | 'fill';
   wrapperProps?: HTMLAttributes<HTMLDivElement>;
+  containerProps?: HTMLAttributes<HTMLDivElement>;
 }
 
-export interface StyledAvatarWrapperProp
-  extends HTMLAttributes<HTMLDivElement> {
+export interface StyledWrapperProp extends HTMLAttributes<HTMLDivElement> {
+  $size: number;
+}
+
+export interface StyledAvatarProp extends HTMLAttributes<HTMLDivElement> {
   $shape: string;
 }
 
-export interface StyledAvatarProp extends ImgHTMLAttributes<HTMLImageElement> {
+export interface StyledImageProp extends ImgHTMLAttributes<HTMLImageElement> {
   $size: number;
   $mode: string;
 }
