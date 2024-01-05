c21c1b138999a080cd1534b3178ec420cb0083b4
kim-hyunjoo <khj9x.x@gmail.com>
2024-01-05 21:20:26 +0900

    Feat: 로그인 id 더미데이터 생성, 추후에 연동 예정

---
+++ src/Pages/DirectMessagePage/DUMMY_DATA.ts
@@ -0,0 +1,3 @@
+export const myId = '658fd0a3be49cd1fd616154d'; // test1
+// export const myId = '65954514e5b0446310bde77e'; //test2 이거 테스트 망한듯 ㅠㅠ
+// export const myId = '659547b5e5b0446310bde7ab'; // test3
