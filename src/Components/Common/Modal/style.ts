83f85c5f15d44040fcbe725f36499565394b02c9
Cho_Ik_Jun <harry960629@naver.com>
2024-01-05 11:44:17 +0900

    Fix: Modal 컴포넌트 wrapper 너비 및 높이 수정 (#42)

--- src/Components/Common/Modal/style.ts
+++ src/Components/Common/Modal/style.ts
@@ -5,8 +5,8 @@ export const StyledModalWrapper = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
