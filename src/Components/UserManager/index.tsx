b37e4f1d64d893c1c832a67c08d6178b7b9151a4
지인혁 <dhgg321@naver.com>
2024-01-07 23:58:15 +0900

    Feat: 유저 관리 컴포넌트
    - offset, limit 상태 관리
    - offset, limit에 따른 전체 사용자 목록을 가져오는 useQuery 훅
    - 현재 접속 중 사용자 목록을 가져오는 useQuery 훅 (2초마다 refetch)

--- src/Components/UserManager/index.tsx
+++ src/Components/UserManager/index.tsx
@@ -1,47 +1,76 @@
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import UserList from './UserList';
-import UserSearchForm from './UserSearchForm';
+// import UserSearchForm from './UserSearchForm';
 import StyledWrapper from './style';
 import { getOnlineUsers, getUsers } from '@/Services/User';
 import { UserType } from '@/Types/UserType';
+import QUERY_KEYS from '@/Constants/queryKeys';
 
 const UserManager = () => {
   const [usersOption, setUsersOption] = useState({ offset: 0, limit: 10 });
   const [allUserList, setAllUserList] = useState<UserType[]>([]);
 
-  const { data: userList } = useQuery({
-    queryKey: ['USER_LIST', usersOption.offset, usersOption.limit],
+  /**
+   * 전체 사용자 목록을 불러오는 API 요청을 수행하는 useQuery 훅
+   * @const
+   * @type {Object}
+   * @property {UserType[]} data.userList - 불러온 사용자 목록
+   * @property {boolean} isLoading - 데이터 로딩 상태
+   */
+  const { data: userList, isLoading } = useQuery({
+    queryKey: [QUERY_KEYS.USER_LIST, usersOption.offset, usersOption.limit],
     queryFn: () => getUsers(usersOption),
   });
 
-  const { data: onlineUserList, isFetching } = useQuery({
-    queryKey: ['ONLINE_USER_LIST'],
+  /**
+   * 현재 접속중인 사용자 목록을 불러오는 API 요청을 수행하는 useQuery 훅
+   * @const
+   * @type {Object}
+   * @property {UserType[]} data.onlineUserList - 현재 접속 중인 사용자 목록
+   * @options {refetchInterval} - 2초마다 API를 재요청하여 데이터를 최신 상태로 유지
+   */
+  const { data: onlineUserList } = useQuery({
+    queryKey: [QUERY_KEYS.ONLINE_USER_LIST],
     queryFn: getOnlineUsers,
     refetchInterval: 2000,
   });
 
-  const loadMoreUsers = () => {
-    setUsersOption((prevOptions) => ({
+  /**
+   * 더 많은 유저를 불러오기 위해 offset과 limit을 증가시키는 함수
+   * 만약 받아온 유저목록이 undefined거나 빈 배열일 경우 offset과 limit를 증가시키지 않음
+   * @function loadMoreUsers
+   * @returns {void} 반환값 없음
+   */
+  const loadMoreUsers = useCallback(() => {
+    if (!userList || userList.length === 0) {
+      return setUsersOption((prevOptions) => ({
+        offset: prevOptions.offset,
+        limit: prevOptions.limit,
+      }));
+    }
+
+    return setUsersOption((prevOptions) => ({
       offset: prevOptions.offset + 10,
       limit: prevOptions.limit + 10,
     }));
-  };
-
-  console.log(userList);
+  }, [userList]);
 
   useEffect(() => {
-    if (userList) {
+    if (userList && userList.length !== 0) {
       setAllUserList((prevList) => [...prevList, ...userList]);
     }
   }, [userList]);
 
   return (
     <StyledWrapper>
-      <UserSearchForm />
+      {/* <UserSearchForm /> */}
       <UserList
+        isLoading={isLoading}
+        isEnd={userList?.length === 0}
         userList={allUserList}
-        onlineUserLIst={onlineUserList || []}
+        onlineUserList={onlineUserList || []}
+        loadMoreUsers={loadMoreUsers}
       />
     </StyledWrapper>
   );
--- src/Components/UserManager/style.ts
+++ src/Components/UserManager/style.ts
@@ -2,15 +2,12 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.section`
   width: 30rem;
-  height: 100vh;
-  background-color: pink;
-  padding: ${({ theme }) => theme.size.small};
-  border-left: 1px solid #ddd;
-
+  height: 100%;
+  padding: 0 0.1rem 0 0;
+  background-color: ${({ theme }) => theme.colors.background};
+  border-left: 1px solid ${({ theme }) => theme.colors.border};
   display: flex;
   flex-direction: column;
-  align-items: center;
-  gap: ${({ theme }) => theme.size.small};
 `;
 
 export default StyledWrapper;
