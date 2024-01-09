e193852c830deb4d696bfc70b0b4f53e4bc21866
Jaewoong Hwang <noa9925@gmail.com>
2024-01-09 10:07:24 +0900

    Fix: 유효성 검사 수정

--- src/Components/SignUp/SignUpForm/validateSignUp.ts
+++ src/Components/SignUp/SignUpForm/validateSignUp.ts
@@ -2,17 +2,17 @@ import validateEmail from '@/Utils/validateEmail';
 import { ValidateSignUpProps } from './type';
 
 const validateSignUp = ({ fullName, email, password }: ValidateSignUpProps) => {
-  const validationErrors: Record<string, string> = {};
+  const validationErrors: Partial<ValidateSignUpProps> = {};
 
-  if (typeof fullName === 'string' && fullName.length > 16) {
-    validationErrors.fullname = '유저명은 16자 이내로 작성해주세요.';
+  if (fullName && typeof fullName === 'string' && fullName.length > 16) {
+    validationErrors.fullName = '유저명은 16자 이내로 작성해주세요.';
   }
 
-  if (typeof email === 'string' && !validateEmail(email)) {
+  if (email && typeof email === 'string' && !validateEmail(email)) {
     validationErrors.email = '유효하지 않은 이메일입니다.';
   }
 
-  if (typeof password === 'string' && password.length < 8) {
+  if (password && typeof password === 'string' && password.length < 8) {
     validationErrors.password = '비밀번호는 최소 8자 이상이어야 합니다.';
   }
 
