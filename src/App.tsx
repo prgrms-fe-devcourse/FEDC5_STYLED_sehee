3b53b345da97b1b0eb64558aacf715fdb1aec2f0
kim-hyunjoo <khj9x.x@gmail.com>
2024-01-11 10:57:15 +0900

    Refactor: App 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { lightTheme, darkTheme } from '@/Styles/Theme';
 import GlobalStyle from '@/Styles/Global';
 import RouteManager from '@/Routes/Router';
-import { login, signUp } from './Services/Auth';
 import { useDarkModeStore } from './Stores';
 
 const queryClient = new QueryClient({
@@ -15,31 +14,10 @@ const queryClient = new QueryClient({
 const App = () => {
   const { isDarkMode } = useDarkModeStore();
 
-  const handleLogin = async () => {
-    const data = await login({ email: 'test1', password: 'test1' });
-    console.log(data);
-  };
-
-  const handleSignUp = async () => {
-    await signUp({ email: 'test6', password: 'test6', fullName: '테스트5' });
-  };
-
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
-        <button
-          type="button"
-          onClick={handleLogin}
-        >
-          버튼
-        </button>
-        <button
-          type="button"
-          onClick={handleSignUp}
-        >
-          버튼2
-        </button>
         <RouteManager />
       </QueryClientProvider>
     </ThemeProvider>
