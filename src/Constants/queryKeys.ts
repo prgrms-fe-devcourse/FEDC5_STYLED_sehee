bc729c466c86e71010a9955f969e67668b08575c
지인혁 <dhgg321@naver.com>
2024-01-12 11:12:40 +0900

    Feat: 사용자 검색 쿼리 키 추가

--- src/Constants/queryKeys.ts
+++ src/Constants/queryKeys.ts
@@ -2,6 +2,7 @@ const QUERY_KEYS = {
   USER_LIST: 'userList',
   ONLINE_USER_LIST: 'onlineUserList',
   NOTIFICATION_LIST: 'notificationList',
+  SEARCH_USER_LIST: 'searchUserList',
 };
 
 export default QUERY_KEYS;
