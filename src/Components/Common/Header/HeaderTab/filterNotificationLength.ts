c9a069239d7196e68dddf53492ce9991b211aec0
지인혁 <dhgg321@naver.com>
2024-01-13 00:12:46 +0900

    Feat: 알림 갯수 필터링 함수 기능 개발

---
+++ src/Components/Common/Header/HeaderTab/filterNotificationLength.ts
@@ -0,0 +1,36 @@
+import { NotificationType } from '@/Types/NotificationType';
+
+const filterNotificationLength = (notifications: NotificationType[]) => {
+  return notifications?.reduce(
+    (acc, notification) => {
+      const { post, follow, comment, message, seen } = notification;
+
+      if (!seen) {
+        return acc;
+      }
+
+      if (post) {
+        acc.postLength += 1;
+      }
+      if (follow) {
+        acc.followLength += 1;
+      }
+      if (comment) {
+        acc.commentLength += 1;
+      }
+      if (message) {
+        acc.messageLength += 1;
+      }
+
+      return acc;
+    },
+    {
+      postLength: 0,
+      followLength: 0,
+      commentLength: 0,
+      messageLength: 0,
+    },
+  );
+};
+
+export default filterNotificationLength;
