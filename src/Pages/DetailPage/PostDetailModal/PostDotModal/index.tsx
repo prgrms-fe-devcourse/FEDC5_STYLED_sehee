298439d759890bf15904192711cde7fb11066680
Cho-Ik-Jun <harry960629@naver.com>
2024-01-15 02:11:14 +0900

    Fix: dev 병합 시 중복 코드 수정

--- src/Pages/DetailPage/PostDetailModal/PostDotModal/index.tsx
+++ src/Pages/DetailPage/PostDetailModal/PostDotModal/index.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from 'styled-components';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Modal from '@/Components/Common/Modal';
 import {
   PostDotModalProps,
@@ -21,7 +21,6 @@ const PostDotModal = ({
 }: PostDotModalProps) => {
   const { colors } = useTheme();
   const { user: authUser } = useAuthUserStore();
-  const { postId } = useParams();
   const navigate = useNavigate();
 
   /**
