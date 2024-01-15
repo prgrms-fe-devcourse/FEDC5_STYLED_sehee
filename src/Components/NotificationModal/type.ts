d3fefe79e08f0c1a22764c8be65d3e4bfb29a2ac
지인혁 <dhgg321@naver.com>
2024-01-15 12:18:46 +0900

    Fix: 댓글 알림 포스트 ID 파싱 데이터 수정

--- src/Components/NotificationModal/type.ts
+++ src/Components/NotificationModal/type.ts
@@ -1,31 +1,14 @@
-import { CommentType } from '@/Types/CommentType';
-import { FollowType } from '@/Types/FollowType';
-import { UserType } from '@/Types/UserType';
+import { NotificationType } from '@/Types/NotificationType';
 
 export interface Props {
   onClose: () => void;
 }
 
-export interface NotificationListType {
+export interface NotificationListType extends NotificationType {
   text: string;
   date: string;
   type: string;
   typeId: string;
-  seen: boolean;
-  _id: string;
-  author: UserType;
-  user: string | UserType;
-  post: string | null;
-  follow?: FollowType | undefined;
-  comment?: CommentType | undefined;
-  message?: string | undefined;
-  createdAt: string;
-  updatedAt: string;
 }
 
 export type CategoryType = '전체' | '메세지' | '댓글' | '팔로우' | '좋아요';
-
-export interface PostSendNotificationsType {
-  messageId: string;
-  receiverId: string;
-}
