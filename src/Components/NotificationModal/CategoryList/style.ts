04ad2fb10a1708b218e2dd8fcd0d9b299e0fdcea
지인혁 <dhgg321@naver.com>
2024-01-17 00:17:24 +0900

    Design: 알림 카테고리 리스트 반응형 gap 넓이 추가

--- src/Components/NotificationModal/CategoryList/style.ts
+++ src/Components/NotificationModal/CategoryList/style.ts
@@ -6,6 +6,10 @@ const StyledCategoryList = styled.ul`
   align-items: center;
   padding: 0.5rem 2rem;
   flex-wrap: wrap;
+
+  @media ${({ theme }) => theme.device.laptop} {
+    gap: 2rem;
+  }
 `;
 
 export default StyledCategoryList;
