e333a309b26d9feafbabf6d97478ff241a4578c8
kim-hyunjoo <khj9x.x@gmail.com>
2024-01-16 19:43:55 +0900

    Refactor: 메세지 관련 react-query 커스텀 훅 refetchInterval 설정 및 리팩토링

--- src/Hooks/Api/Message/index.ts
+++ src/Hooks/Api/Message/index.ts
@@ -12,60 +12,67 @@ import { ConversationType } from '@/Types/ConversationType';
 import QUERY_KEYS from '@/Constants/queryKeys';
 
 export const useFetchConversations = () => {
-  const { data, isLoading, refetch } = useQuery<ConversationType[] | null>({
+  const { data, isFetching, isLoading, refetch } = useQuery<
+    ConversationType[] | null
+  >({
     queryKey: [QUERY_KEYS.CONVERSATIONS],
     queryFn: getConversations,
+    refetchInterval: 2000,
   });
 
   return {
     conversations: data,
     isConversationsLoading: isLoading,
+    isConversationsFetching: isFetching,
     conversationsRefetch: refetch,
   };
 };
 
 export const useFetchMessages = (userId: string) => {
-  const { data, isLoading, refetch } = useQuery<MessageType[] | null>({
-    queryKey: [QUERY_KEYS.MESSAGES],
-    queryFn: async () => {
-      const messages = await getMessages(userId);
-      return messages
-        ? messages.sort(
-            (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt),
-          )
-        : [];
-    },
-    enabled: !!userId,
-  });
+  const { data, isFetching, isFetchedAfterMount, isLoading, refetch } =
+    useQuery<MessageType[] | null>({
+      queryKey: [QUERY_KEYS.MESSAGES],
+      queryFn: async () => {
+        const messages = await getMessages(userId);
+        return messages
+          ? messages.sort(
+              (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt),
+            )
+          : [];
+      },
+      refetchInterval: 2000,
+      enabled: !!userId,
+    });
 
   return {
+    isMessagesFetchedAfterMount: isFetchedAfterMount,
     messages: data,
+    isMessagesFetching: isFetching,
     isMessagesLoading: isLoading,
     messagesRefetch: refetch,
   };
 };
 
-/* 이 밑으로 아직 사용 안하는 중 */
-
-export const useCreateMessage = ({
-  message,
-  receiver,
-}: PostCreateMessageRequestType) => {
-  const { data, isError, isSuccess } = useMutation({
-    mutationFn: () => createMessage({ message, receiver }),
+export const useCreateMessage = (onError?: () => void) => {
+  const { data, mutate, isError, isSuccess, mutateAsync } = useMutation({
+    mutationFn: ({ message, receiver }: PostCreateMessageRequestType) =>
+      createMessage({ message, receiver }),
+    onError,
   });
 
   return {
-    message: data,
+    createdMessage: data,
+    mutateCreateMessage: mutate,
     isCreateMessageError: isError,
     isCreateMessageSuccess: isSuccess,
+    mutateAsync,
   };
 };
 
 export const useReadMessage = () => {
   const queryClient = useQueryClient();
 
-  const { mutate } = useMutation({
+  const { mutate, isError } = useMutation({
     mutationFn: (userId: string) => readMessage(userId),
     onSettled: () => {
       queryClient.refetchQueries({ queryKey: [QUERY_KEYS.CONVERSATIONS] });
@@ -74,5 +81,6 @@ export const useReadMessage = () => {
 
   return {
     mutateReadMessage: mutate,
+    isReadMessageError: isError,
   };
 };
