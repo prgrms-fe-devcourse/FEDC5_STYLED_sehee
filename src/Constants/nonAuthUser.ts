73e0060ecd7ca9470accd02decc2c2ae25abb0cd
kim-hyunjoo <khj9x.x@gmail.com>
2024-01-17 03:55:32 +0900

    Feat: 비회원 Alert 메세지 상수 저장

---
+++ src/Constants/nonAuthUser.ts
@@ -0,0 +1,10 @@
+const NON_AUTH_USER = {
+  ADD_POST: '비회원은 글 작성이 불가능합니다.',
+  FOLLOW: '비회원은 팔로우가 불가능합니다.',
+  MESSAGE: '비회원은 메세지 보내기가 불가능합니다.',
+  LIKE: '비회원은 좋아요가 불가능합니다.',
+  COMMENT: '비회원은 댓글 달기가 불가능합니다.',
+  LOGIN: '로그인 페이지로 이동하시겠습니까?',
+};
+
+export default NON_AUTH_USER;
